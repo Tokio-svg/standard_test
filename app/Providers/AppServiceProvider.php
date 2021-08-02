7a5b4bbe4307eb50f81804d9bfba25746d007bf8
TOKIO <tokkipt935@gmail.com>
2021-08-02 11:08:25 +0900

    heroku setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         // ローカル環境でない場合はリンクをhttpsで出力する
         if (!app()->islocal()) {
             \URL::forceScheme('https');
